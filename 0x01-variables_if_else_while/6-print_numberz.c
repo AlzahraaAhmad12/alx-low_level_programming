c5595e83f56091a3257367f8f2e19d16c7424fe7
AlzahraaAhmad12 <zahraaahmad128@gmail.com>
2023-05-04 15:45:04 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ *main-  numbers from 0 to 9 .
+ * Return:0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+{
+putchar(num + '0');
+}
+putchar('\n');
+return (0);
+}
