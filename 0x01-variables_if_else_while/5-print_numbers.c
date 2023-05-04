d1997d8d9bcd5c4122ec14549d56546c9cc900d6
AlzahraaAhmad12 <zahraaahmad128@gmail.com>
2023-05-04 15:40:53 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ *main-  numbers from 0 to 9 .
+ * Return:0
+ */
+int main(void)
+{
+int num;
+for (num = 0; i <= 9; i++)
+{
+printf("%d", num);
+}
+putchar('\n');
+return (0);
+}
