548a2c7f6488333cb5d37db3962430062d703094
AlzahraaAhmad12 <zahraaahmad128@gmail.com>
2023-05-04 16:45:18 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ *main- Entry POint.
+ * Return:0
+ */
+int main(void)
+{
+char arr[] = "_putchar";
+int c;
+
+for (c = 0; c < 8; c++)
+{
+_putchar(arr[c]);
+}
+_putchar('\n');
+return (0);
+}
